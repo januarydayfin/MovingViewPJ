e0b4d09f1f4e751a2d7002359befd47230aff50e
januarydayfin <krayman7250@gmail.com>
2022-04-01 14:36:10 +0700

    Выполнено

--- app/src/main/java/com/krayapp/movingviewpj/MainActivity.kt
+++ app/src/main/java/com/krayapp/movingviewpj/MainActivity.kt
@@ -3,20 +3,16 @@ package com.krayapp.movingviewpj
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
-import android.content.Context
-import android.graphics.Path
 import android.graphics.Point
 import android.os.Bundle
-import android.util.AttributeSet
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private val car: View by lazy { initCar() }
-
+    private val width: Int by lazy { getWindowSize()[0] }
+    private val height: Int by lazy { getWindowSize()[1] }
     private val viewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,43 +20,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         clickListener()
         viewModel.mutableLiveData.observe(this) {
-            checkCarInCorner()
-            moveCar(it)
+            if (!checkCarInCorner()) {
+                moveZero()
+            } else {
+                moveCar(it)
+            }
         }
         getWindowSize()
     }
+
     private fun initCar(): View = findViewById(R.id.car_view)
 
     private fun clickListener() {
         car.setOnClickListener {
             viewModel.calculateCar()
         }
-
     }
 
-    private fun getWindowSize() {
+    private fun getWindowSize(): IntArray {
         val size = Point()
         windowManager.defaultDisplay.getSize(size)
         val height = size.y
         val width = size.x
-        val arrayList = ArrayList<Int>()
-        arrayList.add((width / 2) - 100) //чтобы элемент не выходил за границу - вычитаю 100 либо 200
-        arrayList.add((height / 2) - 200)
-        viewModel.setSize(arrayList)
+        val intArray = IntArray(2)
+        intArray[0] =
+            ((width / 2) - 100) //чтобы элемент не выходил за границу - вычитаю 100 либо 200
+        intArray[1] = ((height / 2) - 200)
+        viewModel.setSize(intArray)
+        return intArray
     }
 
-    /**
-     * Узнать крайние координаты, и каждый раз проверять  где находится view
-     */
     @SuppressLint("Recycle")
-    private fun moveCar(array:IntArray) {
-        val path = Path().apply {
-            arcTo(0f, 0f, 1000f, 1000f, 270f, -180f, true)
-        }
-        val arcAnimation = ObjectAnimator.ofFloat(car, View.X, View.Y, path).apply {
-            duration = 2000
-        }
-        val randomValue = Random.nextInt(-200, -100).toFloat()
+    private fun moveCar(array: IntArray) {
         val animX = ObjectAnimator.ofFloat(car, "TranslationX", array[0].toFloat())
         val animY = ObjectAnimator.ofFloat(car, "TranslationY", array[1].toFloat())
         val animator = AnimatorSet()
@@ -68,10 +59,19 @@ class MainActivity : AppCompatActivity() {
         animator.start()
     }
 
-    private fun checkCarInCorner() {
+    private fun moveZero() {
+        val animX = ObjectAnimator.ofFloat(car, "TranslationX", 0.toFloat())
+        val animY = ObjectAnimator.ofFloat(car, "TranslationY", 0.toFloat())
+        val animator = AnimatorSet()
+        animator.playSequentially(animX, animY)
+        animator.start()
+    }
+
+    private fun checkCarInCorner(): Boolean {
         val carLocation = IntArray(2)
         car.getLocationOnScreen(carLocation)
-        viewModel.checkCarInCorner(carLocation)
+        return (carLocation[0] > width || carLocation[0] < -width
+                && carLocation[1] > height || carLocation[1] < -height)
     }
 
 
--- app/src/main/java/com/krayapp/movingviewpj/MainViewModel.kt
+++ app/src/main/java/com/krayapp/movingviewpj/MainViewModel.kt
@@ -9,19 +9,17 @@ class MainViewModel : ViewModel() {
     private val _mutableLiveData = MutableLiveData<IntArray>()
     val mutableLiveData: LiveData<IntArray>
         get() = _mutableLiveData
-    private var inCorner: Boolean = false
 
     private var screenWidth: Int = 0
     private var screenHeight: Int = 0
 
-    fun setSize(list: ArrayList<Int>) {
+    fun setSize(list: IntArray) {
         screenWidth = list[0]
         screenHeight = list[1]
     }
 
 
     fun calculateCar() {
-        if (inCorner) {
             var carMoveArray = IntArray(2)
         when(Random.nextInt(1, 4)){
             1 ->{
@@ -38,7 +36,7 @@ class MainViewModel : ViewModel() {
             }
         }
             _mutableLiveData.postValue(carMoveArray)
-        }
+
     }
 
     private fun toTopLeft():IntArray = intArrayOf(-screenWidth, screenHeight)
@@ -46,10 +44,4 @@ class MainViewModel : ViewModel() {
     private fun toBotLeft():IntArray = intArrayOf(-screenWidth,-screenHeight)
     private fun toBotRight():IntArray = intArrayOf(screenWidth,-screenHeight)
 
-
-
-    fun checkCarInCorner(carLocation: IntArray) {
-        inCorner = (carLocation[0] > screenWidth || carLocation[0] < -screenWidth
-                && carLocation[1] > screenHeight || carLocation[1] < -screenHeight)
-    }
 }
